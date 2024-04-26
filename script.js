cd2ffa6ebac719e07ff2beacc3f0a4c08608d13f
serojote <165111431+serojote@users.noreply.github.com>
2024-04-26 15:07:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const clickBtn = document.getElementById('clickBtn');
-  const scoreDisplay = document.getElementById('score');
   let score = 0;
 
   clickBtn.textContent = score;
 
   clickBtn.addEventListener('click', () => {
     score++;
-    scoreDisplay.textContent = score;
     clickBtn.textContent = score;
   });
 });
